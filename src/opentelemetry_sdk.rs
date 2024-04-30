23c6da75211fca0987c590a4e2eeb8737c1332dc
Cijo Thomas <cijo.thomas@gmail.com>
2024-04-29 17:44:54 -0700

    leverage builtin mechanisms in tracing to keep track of span data

--- src/opentelemetry_sdk.rs
+++ src/opentelemetry_sdk.rs
@@ -2,7 +2,7 @@ use std::{cell::RefCell, collections::HashMap, sync::Mutex, time::SystemTime};
 
 use rand::{rngs, Rng, SeedableRng};
 use tracing::{field::Visit, span};
-use tracing_subscriber::{layer::Context, Layer};
+use tracing_subscriber::{layer::Context, registry::LookupSpan, Layer};
 
 thread_local! {
     static CURRENT_RNG: RefCell<rngs::SmallRng> = RefCell::new(rngs::SmallRng::from_entropy());
@@ -35,6 +35,7 @@ pub struct OTelSpan {
     pub start_time: SystemTime,
     pub end_time: SystemTime,
     pub attributes: HashMap<String, String>,
+    pub is_recording: bool,
 }
 
 impl OTelSpan {
@@ -42,6 +43,7 @@ impl OTelSpan {
         name: String,
         trace_id: Option<TraceId>,
         parent_span_id: Option<SpanId>,
+        is_recording: bool,
     ) -> OTelSpan {
         OTelSpan {
             name,
@@ -53,6 +55,7 @@ impl OTelSpan {
             start_time: SystemTime::now(),
             end_time: SystemTime::now(),
             attributes: HashMap::new(),
+            is_recording: is_recording,
         }
     }
 }
@@ -69,8 +72,20 @@ impl Visit for OTelSpan {
     }
 }
 
+pub trait ShouldSample {
+    fn should_sample(&self, trace_id: &TraceId) -> bool;
+}
+
+pub struct OTelSampler;
+
+impl ShouldSample for OTelSampler {
+    fn should_sample(&self, _trace_id: &TraceId) -> bool {
+        true
+    }
+}
+
 pub struct OpenTelemetrySdk {
-    spans: Mutex<HashMap<span::Id, OTelSpan>>,
+    sampler: OTelSampler,
 }
 
 impl Default for OpenTelemetrySdk {
@@ -82,51 +97,82 @@ impl Default for OpenTelemetrySdk {
 impl OpenTelemetrySdk {
     pub fn new() -> OpenTelemetrySdk {
         OpenTelemetrySdk {
-            spans: Mutex::new(HashMap::new()),
+            sampler: OTelSampler,
         }
     }
 }
 
 impl<S> Layer<S> for OpenTelemetrySdk
 where
-    S: tracing::Subscriber,
+    S: tracing::Subscriber + for<'span> LookupSpan<'span>,
 {
     fn on_new_span(&self, attrs: &span::Attributes<'_>, id: &span::Id, ctx: Context<'_, S>) {
-        // This is where Samplers are called, and its results can be used to populate fields like is_recording
-        // Spans are stored in a self managed HashMap for demo purposes. But this could
-        // be replaced with span.extensions like done by `tracing-opentelemetry`.
+        let span = ctx.span(id).expect("Span expected here");
+        let mut extensions = span.extensions_mut();
+
         let parent_span = ctx.current_span();
         if let Some(parent_id) = parent_span.id() {
-            let parent_trace_id = self.spans.lock().unwrap().get(parent_id).unwrap().trace_id;
-            let parent_span_id = self.spans.lock().unwrap().get(parent_id).unwrap().span_id;
+            // parent span exists.
+            // reuse traceid for the new span being created
+            // and store parent span id to the new span being created.
+            let parent_span = ctx.span(&parent_id).expect("Parent span expected here");
+            let mut parent_extensions = parent_span.extensions_mut();
+            let parent_span = parent_extensions
+                .get_mut::<OTelSpan>()
+                .expect("Parent span data expected here");
+
+            let parent_trace_id = parent_span.trace_id;
+            let parent_span_id = parent_span.span_id;
+            let sampling_result = self.sampler.should_sample(&parent_trace_id);
             let mut span = OTelSpan::new(
                 attrs.metadata().name().to_string(),
                 Some(parent_trace_id),
                 Some(parent_span_id),
+                sampling_result,
             );
             attrs.record(&mut span);
-            self.spans.lock().unwrap().insert(id.clone(), span);
+
+            // store span in span extension.
+            extensions.insert(span);
         } else {
-            let mut span = OTelSpan::new(attrs.metadata().name().to_string(), None, None);
+            // parent span does not exist.
+            // TODO: This is where remote parent's span context needs to be extracted, if any.
+            let trace_id_to_be_created_span =
+                CURRENT_RNG.with(|rng| TraceId::from(rng.borrow_mut().gen::<u128>()));
+            let sampling_result = self.sampler.should_sample(&trace_id_to_be_created_span);
+            let mut span = OTelSpan::new(
+                attrs.metadata().name().to_string(),
+                Some(trace_id_to_be_created_span),
+                None,
+                sampling_result,
+            );
             attrs.record(&mut span);
-            self.spans.lock().unwrap().insert(id.clone(), span);
+
+            // store span in span extension.
+            extensions.insert(span);
         }
 
         // This is where SpanProcessors' OnBegin will be called.
     }
 
-    fn on_close(&self, id: span::Id, _ctx: Context<'_, S>) {
-        let mut span = self.spans.lock().unwrap().remove(&id).unwrap();
+    fn on_close(&self, id: span::Id, ctx: Context<'_, S>) {
+        let span = ctx.span(&id).expect("Span expected here");
+        let mut extensions = span.extensions_mut();
+        let mut span = extensions.remove::<OTelSpan>().expect("Span expected here");
         span.end_time = SystemTime::now();
-        // Simply printing the span for now.
-        // This is where SpanProcessors' OnEnd will be called.
-        // SpanProcessors can pass Spans to exporter(s) which can export in OTLP format/others.
-        println!("Span {:?}", span)
+        println!("Span {:?}", span);
+        if span.is_recording {
+            // This is where SpanProcessors' OnEnd will be called.
+            // SpanProcessors can pass Spans to exporter(s) which can export in OTLP format/others.
+        }
     }
 
-    fn on_record(&self, span: &span::Id, values: &span::Record<'_>, _ctx: Context<'_, S>) {
-        if let Some(existing_span) = self.spans.lock().unwrap().get_mut(&span) {
-            values.record(existing_span);
-        }
+    fn on_record(&self, span: &span::Id, values: &span::Record<'_>, ctx: Context<'_, S>) {
+        let span = ctx.span(&span).expect("Span expected here");
+        let mut extensions = span.extensions_mut();
+        let existing_span = extensions
+            .get_mut::<OTelSpan>()
+            .expect("Span expected here");
+        values.record(existing_span);
     }
 }
